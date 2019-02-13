e15939ffaca151ba0f40378a122c3b25c7e56481
maysatome <artemmelnykphd@gmail.com>
2019-02-13 22:04:27 +0000

    elegant solution

--- exerc01.cpp
+++ exerc01.cpp
@@ -1,4 +1,12 @@
 //Faça um programa que receba quatro números inteiros, calcule e mostre a soma desses números.
 
-#include "std_lib_facilities.h"
-int main(){int n1(5), n2(6), n3(3), n4(1);cout << n1 + n2 + n3 + n4; return 0;}
+#include<iostream>
+using namespace std;
+int main(){int n1(5), n2(6), n3(3), n4(1);cout << n1 + n2 + n3 + n4<<endl; return 0;}
+/* Compiling + launching and execution are depicted:
+gitpod /workspace/partum $ make exerc01
+g++     exerc01.cpp   -o exerc01
+gitpod /workspace/partum $ ./exerc01
+15
+gitpod /workspace/partum $
+*/

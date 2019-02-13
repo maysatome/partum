8cb4f3b57ea2ce7bbdb563c2b644a2bb6b750c4d
maysatome <47578639+maysatome@users.noreply.github.com>
2019-02-13 22:57:19 +0100

    Create exerc01.cpp

---
+++ exerc01.cpp
@@ -0,0 +1,4 @@
+//Faça um programa que receba quatro números inteiros, calcule e mostre a soma desses números.
+
+#include "std_lib_facilities.h"
+int main(){int n1(5), n2(6), n3(3), n4(1);cout << n1 + n2 + n3 + n4; return 0;}

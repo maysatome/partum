789abd932cfbd4ddb49d3c6b7d4a10373aee1553
maysatome <47578639+maysatome@users.noreply.github.com>
2019-02-14 09:55:15 -0200

    Create maysa.cpp

---
+++ maysa.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+//faça um programa que receba três notas, calcule e mostre a média aritmética entre elas;
+
+
+int main ()
+{
+int n1(8), n2(6), n3(10); 
+float media_aritmetica; 
+
+media_aritmetica=(n1+n2+n3)/3;
+printf("Media_aritmetica e= (%d + %d + %d / 3) = %f", n1,n2,n3, media_aritmetica); 
+
+return 0;
+	
+	
+}

57ed684192933e4eef2911626d9ddb20fbb62911
maysatome <artemmelnykphd@gmail.com>
2019-02-15 15:02:51 +0000

    some improuvements

--- exerc10.cpp
+++ exerc10.cpp
@@ -1,7 +1,7 @@
 //Make a program that calculates and shows the area of a circle. It is known that: Area = p * r2.
 
 #include<iostream>
-#define PI 3.1415 // C-style
+#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286 // C-style
 
 int main()
 {
@@ -9,3 +9,4 @@ int main()
     printf("Area is: %.6f\n", PI*r*r);// C-style
     return 0;
 }
+// https://www.tutorialspoint.com/c_standard_library/math_h.htm

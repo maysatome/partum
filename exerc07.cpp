7d0a1e718322b3332a35d8c6fb77c0cd1325236d
maysatome <artemmelnykphd@gmail.com>
2019-02-15 10:59:20 +0000

    advances

--- exerc07.cpp
+++ exerc07.cpp
@@ -1,9 +1,4 @@
-// Make a program that receives the base salary (minimum wage/ monthly remuneration)
-// of an employee, calculate and show your salary receivable,
-// knowing that the employee has a bonus of $ 50
-// (Gained 50 'reais' more. “$” it’s meant “reais”, which represents brazilian Money)
-// and pays (Here, he lost some Money because he need to pay interest rate equal 10%)
-// a 10% tax on the base salary.
+Make a program that receives the value of a deposit and the value of the interest rate, calculate and show the value of the yield and the total value after the yield. ##“That’s seems confusing to me. Maybe it’s better to give up this”##.
 
 #include<iostream>
 using namespace std;
---
+++ exerc10.cpp
@@ -0,0 +1,11 @@
+//Make a program that calculates and shows the area of a circle. It is known that: Area = p * r2.
+
+#include<iostream>
+#define PI 3.1415 // C-style
+
+int main()
+{
+    double r = 0.1;// C-style
+    printf("Area is: %.6f\n", PI*r*r);// C-style
+    return 0;
+}

9a9a78c81be6e63c32b59262433d549fa39b4fee
maysatome <47578639+maysatome@users.noreply.github.com>
2019-02-15 14:15:44 +0100

    Update maysa06.cpp

--- maysa06.cpp
+++ maysa06.cpp
@@ -2,29 +2,16 @@
 //Make a program that receives the base salary (minimum wage/ monthly remuneration) of an employee, calculate and show the salary to receive, 
 //knowing that the employee has a bonus (gain more money) of 5% on the base salary and pays 7% tax (interest rate) on this salary.
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 using namespace std;
 int main   ()  
-
-
 {
-float base_salario(0.0);
-float novo_salario(0.0);
-float salario_after(0.0);
-float salario_geral(0.0);
- 
-//novo_salario= (base_salario*0.05)*0.07;
-
-cout<< "Digite o base_salario e clique em Enter";
-cin >> base_salario; 
-cout<<"digite o percentual de aumento e clique em Enter";
-cin>> salario_after;
-cout<<"digite o percentual de desconto e clique em Enter";
-cin>>salario_geral;
-
-salario_geral= (base_salario*0.05)*0.07;
-
-
-
+	float base_salario(0.0), novo_salario(0.0), salario_after(0.0), salario_geral(0.0);
+ 	cout<< "Digite o base_salario e clique em Enter";
+	cin >> base_salario; 
+	cout<<"digite o percentual de aumento e clique em Enter";
+	cin>> salario_after;
+	cout<<"digite o percentual de desconto e clique em Enter";
+	cin>>salario_geral;
+	salario_geral= (base_salario*0.05)*0.07;
 	return 0;
 }

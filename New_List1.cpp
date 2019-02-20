a3188263bf7e706a2fc884b40ff4a19b1036a595
maysatome <47578639+maysatome@users.noreply.github.com>
2019-02-20 14:17:40 +0100

    Update New_List1.cpp

--- New_List1.cpp
+++ New_List1.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main ()
 {
 
-float valor (0.0);
-float dec (0.0), cen(0.0), mil(0.0);
+double valor (0.0);
+double dec (0.0), cen(0.0), mil(0.0);
 
 
 
@@ -15,9 +15,9 @@ cout <<"Por favor, digite o metro e clique em Enter: ";
 cin >> valor;
 
 
-dec= valor*10;
-cen=valor*100;
-mil=valor*1000;
+dec= valor*10.0;
+cen=valor*100.0;
+mil=valor*1000.0;
 
 
  printf ("O valor sera %f * 10 = %f\n", valor, dec); // eu sei que é um valor interiro, mas e quando eu não sei??

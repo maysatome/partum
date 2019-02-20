dacc2ca4bcb7cac87228adaf1229fd38af9224c7
maysatome <47578639+maysatome@users.noreply.github.com>
2019-02-20 08:05:41 -0300

    Create New_List1.cpp

---
+++ New_List1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main ()
+{
+
+float valor (0.0);
+int dec (0.0), cen(0.0), mil(0.0);
+
+
+
+cout <<"Por favor, digite o metro e clique em Enter";
+cin >> valor;
+
+
+dec= valor*10;
+cen=valor*100;
+mil=valor*1000;
+
+
+ printf ("O valor sera %f * 10 = %d\n", valor, dec); // eu sei que é um valor interiro, mas e quando eu não sei??
+ printf ("O valor sera %f * 100 = %d\n", valor, cen);
+ printf ("O valor sera %f * 1000 = %d\n", valor, mil);
+ 
+ 
+ return 0; 
+}

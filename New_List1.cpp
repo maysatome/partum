07fe660f9be04b5a579a63bfa45bc3bc8cbfa866
maysatome <47578639+maysatome@users.noreply.github.com>
2019-02-20 14:08:03 +0100

    Update New_List1.cpp

--- New_List1.cpp
+++ New_List1.cpp
@@ -7,7 +7,7 @@ int main ()
 {
 
 float valor (0.0);
-int dec (0), cen(0), mil(0);
+float dec (0.0), cen(0.0), mil(0.0);
 
 
 
@@ -20,9 +20,9 @@ cen=valor*100;
 mil=valor*1000;
 
 
- printf ("O valor sera %f * 10 = %d\n", valor, dec); // eu sei que é um valor interiro, mas e quando eu não sei??
- printf ("O valor sera %f * 100 = %d\n", valor, cen);
- printf ("O valor sera %f * 1000 = %d\n", valor, mil);
+ printf ("O valor sera %f * 10 = %f\n", valor, dec); // eu sei que é um valor interiro, mas e quando eu não sei??
+ printf ("O valor sera %f * 100 = %f\n", valor, cen);
+ printf ("O valor sera %f * 1000 = %f\n", valor, mil);
  
  
  return 0; 
